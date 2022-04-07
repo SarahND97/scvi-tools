46f011f8ea4844d7532880ef13ee42c0f80448ca
Sarah <sarahnd97@gmail.com>
2022-04-07 09:50:14 +0200

    change to run on server

--- cross_valid_alvis.py
+++ cross_valid_alvis.py
@@ -4,7 +4,6 @@ from sklearn.metrics import adjusted_rand_score as ARI
 from scvi.data._built_in_data._pbmc import _load_pbmc_dataset
 from scvi.data._anndata import _setup_anndata
 import scanpy as sc
-import sys
 import numpy as np
 
 def clustering_scores(labels_true, labels_pred):
@@ -26,7 +25,6 @@ def cross_valid_hybrid(learning_rate, hidden_layers, size_hidden_layer, gene_ind
         train_i = data_.pop(i)
         model_ = model.HYBRIDVI(adata=train_i, gene_indexes=gene_indexes_von_mises, n_hidden=size_hidden_layer, n_layers=hidden_layers)
         model_.train(lr=learning_rate)
-        # TODO: create a get_latent specific for hybridVI
         latent = model_.get_latent_representation(adata=test_i, hybrid=True)
         test_i.obsm["X_scvi"] = latent
         sc.pp.neighbors(test_i, n_neighbors=10, n_pcs=40, use_rep="X_scvi")
@@ -35,6 +33,7 @@ def cross_valid_hybrid(learning_rate, hidden_layers, size_hidden_layer, gene_ind
         pred = [int(x) for x in pred]
         scores_leiden = clustering_scores(test_i.obs["labels"], pred)
         result = [scores_leiden[0], scores_leiden[1]]
+        result = [0, 0]
         results.append(result)
         average_nmi = average_nmi + scores_leiden[0] 
         average_ari = average_ari + scores_leiden[1]
@@ -47,6 +46,18 @@ def cross_valid_hybrid(learning_rate, hidden_layers, size_hidden_layer, gene_ind
     f.close()
     f2.close()
     print(average)
+
+def start(line_nr):
+    file = "input/parameters.in"
+    f = open(file, "r")
+    lines = f.readlines()
+    line = lines[line_nr].split()
+    print(line)
+    learning_rate = float(line[0])
+    hidden_layers = int(line[1])
+    size_hidden_layer = int(line[2])
+    cross_valid_hybrid(learning_rate, hidden_layers, size_hidden_layer, gene_indexes_von_mises, data_cross, K_cross)
+    f.close()
 
 adata = _load_pbmc_dataset(run_setup_anndata=False)
 K_cross = 3
@@ -61,8 +72,6 @@ adata.var["von_mises"] = "false"
 for gene in cell_cycle_genes:
     adata.var.loc[adata.var["gene_symbols"] == gene, "von_mises"] = "true"
 gene_indexes_von_mises = np.where(adata.var['von_mises'] == "true")[0]
-
-
 adata_cross = adata[:int((len(adata)*2)/K_cross), :]
 adata_train_best_model = adata[int((len(adata)*2)/K_cross):, :]
 size = int(len(adata_cross)/K_cross)
@@ -76,14 +85,6 @@ for d in range(len(data)):
     data[d] = da 
 
 data_cross = [data[1], data[2], data[3]]
-file = "input/parameters.in"
-line_nr = int(sys.argv[1])
-f = open(file, "r")
-lines = f.readlines()
-line = lines[line_nr].split()
-print(line)
-learning_rate = float(line[0])
-hidden_layers = int(line[1])
-size_hidden_layer = int(line[2])
-cross_valid_hybrid(learning_rate, hidden_layers, size_hidden_layer, gene_indexes_von_mises, data_cross, K_cross)
-f.close()
+
+for i in range(75):
+    start(i)
